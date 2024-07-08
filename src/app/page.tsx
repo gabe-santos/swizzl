6862a635acdd5e447ab1dbff556f1915e9a4ab69
Gabe <28gabe@gmail.com>
2024-07-08 13:38:57 -0700

    cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-import DrinksGrid from '@/components/drinks-grid';
-import Nav from '@/components/nav';
-import RandomDrinkBtn from '@/components/random-drink-btn';
-import SearchBar from '@/components/search-bar';
-import { getDrinks } from '@/lib/queries/drinks';
+import DrinksGrid from "@/components/drinks-grid";
+import { getDrinks } from "@/lib/queries/drinks";
 
 export default async function Home({
-	searchParams,
+  searchParams,
 }: {
-	searchParams?: { query?: string };
+  searchParams?: { query?: string };
 }) {
-	const query = searchParams?.query || '';
-	const res = await getDrinks(query);
-	const drinks = res.drinks;
+  const query = searchParams?.query || "";
+  const res = await getDrinks(query);
+  const drinks = res.drinks;
 
-	return (
-		<>
-			<DrinksGrid drinks={drinks} />
-		</>
-	);
+  return (
+    <>
+      <DrinksGrid drinks={drinks} />
+    </>
+  );
 }
--- src/components/favorite-btn.tsx
+++ src/components/favorite-btn.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@nextui-org/react';
-import { Heart } from 'lucide-react';
+import { Button } from "@nextui-org/react";
+import { Heart } from "lucide-react";
 
 export default function FavoriteBtn() {
-	return (
-		<form action=''>
-			<Button variant='flat' startContent={<Heart color='#ef4444' />}>
-				Add to Favorites
-			</Button>
-		</form>
-	);
+  return (
+    <form action="">
+      <Button variant="flat" startContent={<Heart color="#ef4444" />}>
+        Add to Favorites
+      </Button>
+    </form>
+  );
 }
