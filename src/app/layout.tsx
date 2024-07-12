02545da9119fd3860e3e9872a42e6883ccbb5288
Gabe Santos <28gabe@gmail.com>
2024-07-12 11:28:52 -0700

    fixes emoji favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Fraunces } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import clsx from "clsx";
+import RandomDrinkBtn from "@/components/buttons/random-drink-btn";
+import Nav from "@/components/ui/nav";
+import SearchBar from "@/components/ui/search-bar";
 
 const fraunces = Fraunces({ subsets: ["latin"] });
 
@@ -21,7 +24,7 @@ export default function RootLayout({
       <head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>🍹</svg>"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍹</text></svg>"
         />
       </head>
       <body className={clsx(fraunces.className, "text-base font-light")}>
